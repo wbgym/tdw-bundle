f77f844cded6b5aaadc75682a7b47f7debdf04f8
konstantin-ebeling <115927484+konstantin-ebeling@users.noreply.github.com>
2023-01-19 16:35:32 +0100

    fix csv parsing, update grade choice amounts, formatting

--- src/Resources/contao/modules/ModuleTdwChoice.php
+++ src/Resources/contao/modules/ModuleTdwChoice.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Contao Open Source CMS
@@ -24,7 +24,7 @@ namespace WBGym;
  * @author     Erik Ziegler 
  */
 
-class ModuleTdwChoice extends \Module 
+class ModuleTdwChoice extends \Module
 {
 
 	/**
@@ -33,25 +33,24 @@ class ModuleTdwChoice extends \Module
 	 */
 	protected $strTemplate = 'mod_tdw_choice';
 	protected $arrChoicesAmount = array
-		(
-			'5'  => 4,
-			'6'  => 4,
-			'7'  => 7,
-			'8'  => 7,
-			'9'  => 7,
-			'10' => 7,
-			'11' => 6,
-			'12' => 6,
-		);
+	(
+		'5' => 5,
+		'6' => 5,
+		'7' => 6,
+		'8' => 6,
+		'9' => 7,
+		'10' => 7,
+		'11' => 7,
+		'12' => 7,
+	);
 
-     /**
+	/**
 	 * Generate the module
 	 */
 	public function generate()
 	{
-		
-		if (TL_MODE == 'BE')
-		{
+
+		if (TL_MODE == 'BE') {
 			$objTemplate = new \BackendTemplate('be_wildcard');
 
 			$objTemplate->wildcard = '### Tag der Wissenschaft Wahl ###';
@@ -62,125 +61,126 @@ class ModuleTdwChoice extends \Module
 
 			return $objTemplate->parse();
 		}
-		
+
 		return parent::generate();
 	}
-	 
+
 
 	protected function compile()
 	{
-		$this->import('FrontendUser','User');
-		$this->import('Database'); 
-        
-		if(!FE_USER_LOGGED_IN) return false;
-		
-		if($_POST['FORM_SUBMIT'] == 'tdw-choice') {
-		
+		$this->import('FrontendUser', 'User');
+		$this->import('Database');
+
+		if (!FE_USER_LOGGED_IN)
+			return false;
+
+		if ($_POST['FORM_SUBMIT'] == 'tdw-choice') {
+
 			$arrChoices = array();
-			
-			foreach($_POST as $key => $val) {
-				if (strpos($key,'lecture') !== false) {
+
+			foreach ($_POST as $key => $val) {
+				if (strpos($key, 'lecture') !== false) {
 					array_push($arrChoices, $val);
 				}
 			}
-			
+
 			if (count($arrChoices) == $this->arrChoicesAmount[WBGym::grade()]) {
-				
+
 				// Bereits gewählte Vorträge laden
 				$rows = $this->Database->prepare("SELECT lecture FROM tl_tdwChoices WHERE student = ?")
-									   ->execute($this->User->id)
-									   ->fetchAllAssoc();
-				
-				
-				$alreadyChosen = array();
-				
-				if (count($rows) > 0)
-					foreach($rows as $row) 
-						array_push($alreadyChosen,$row['lecture']);
-			
-				// Elemente aus den bereits gew�hlten Vortr�gen die nicht in den neu gew�hlten Enthalten sind, werden gel�scht!
-				$delChoices = array_diff($alreadyChosen, $arrChoices); 			  
-				if (count($delChoices) > 0)
-					foreach($delChoices as $delChoice) 
-						$this->Database->prepare("DELETE FROM tl_tdwChoices WHERE student = ? AND lecture = ?")
-									   ->execute($this->User->id, $delChoice);
+					->execute($this->User->id)
+					->fetchAllAssoc();
 
-				
-				// Alle weitere Vortr�ge hinzuf�gen
-				foreach($arrChoices as $choice) 	
-					if(!in_array($choice, $alreadyChosen))
-						$this->Database->prepare("INSERT INTO tl_tdwChoices (tstamp,student,lecture) VALUES (?,?,?)")->execute(time(),$this->User->id,$choice);
-						$this->Template->message = $GLOBALS['TL_LANG']['MSC']['success'];
-				
+
+				$alreadyChosen = array();
+
+				if (count($rows) > 0)
+					foreach ($rows as $row)
+						array_push($alreadyChosen, $row['lecture']);
+
+				// Elemente aus den bereits gew�hlten Vortr�gen die nicht in den neu gew�hlten Enthalten sind, werden gel�scht!
+				$delChoices = array_diff($alreadyChosen, $arrChoices);
+				if (count($delChoices) > 0)foreach ($delChoices as $delChoice)
+						$this->Database->prepare("DELETE FROM tl_tdwChoices WHERE student = ? AND lecture = ?")
+							->execute($this->User->id, $delChoice); // Alle weitere Vortr�ge hinzuf�gen
+				foreach ($arrChoices as $choice)
+					if (!in_array($choice, $alreadyChosen))
+						$this->Database->prepare("INSERT INTO tl_tdwChoices (tstamp,student,lecture) VALUES (?,?,?)")->execute(time(), $this->User->id, $choice);
+				$this->Template->message = $GLOBALS['TL_LANG']['MSC']['success'];
+
 			} else {
 				$this->Template->error = $GLOBALS['TL_LANG']['MSC']['choice_amount_error'];
 			}
-			
+
 		}
-		
+
 		//Get Student ID from Bicher List
-		$strCsvFile = TL_ROOT.'/files/tdw/input_files/T_Schueler_fuer_Schuelerwahl.csv';
-		if(file_exists($strCsvFile)) {
+		$strCsvFile = TL_ROOT . '/files/tdw/input_files/T_Schueler_fuer_Schuelerwahl.csv';
+		if (file_exists($strCsvFile)) {
 			$delimiter = ';';
-			$handle = fopen($strCsvFile,'r');
+			$handle = fopen($strCsvFile, 'r');
 			$row = 1;
-			while(($data = fgetcsv($handle,1000,$delimiter)) !== false) {
-				if ($row > 1 && 
-					$data[1] == $this->User->lastname && 
-					$data[2] == $this->User->firstname && 
-					$data[3] == date('d.m.Y',$this->User->dateOfBirth) 
+			while (($data = fgetcsv($handle, 1000, $delimiter)) !== false) {
+				if (
+					$row > 1 &&
+					$data[1] == $this->User->lastname &&
+					$data[2] == $this->User->firstname &&
+					$data[3] == date(
+						'd.m.Y',
+						$this->User->dateOfBirth
+					)
 				) {
 					$stIdNew = $data[0];
 				}
 				$row++;
 			}
 		}
-		
+
 		//Find courses that the student visited last year
-		$strCsvFile = TL_ROOT.'/files/tdw/input_files/T_Schueler_Themen_Vorjahr.csv';
-		if(file_exists($strCsvFile)) {
+		$strCsvFile = TL_ROOT . '/files/tdw/input_files/T_Schueler_Themen_Vorjahr.csv';
+		if (file_exists($strCsvFile)) {
 			$delimiter = ';';
-			$handle = fopen($strCsvFile,'r');
+			$handle = fopen($strCsvFile, 'r');
 			$row = 1;
-			
-			while(($data = fgetcsv($handle,1000,$delimiter)) !== false) {
+
+			while (($data = fgetcsv($handle, 1000, $delimiter)) !== false) {
 				if ($data[0] == $stIdNew) {
 					$arrLectionsLastYear[] = $data[1];
 				}
 				$row++;
 			}
 		}
-		
+
 		$arrAllLectures = $this->Database->prepare("SELECT * FROM tl_tdwLectures")->execute()->fetchAllAssoc();
 		$arrLectures = array();
-		
-		foreach($arrAllLectures as $lecture) {
-			
-			$bn = pow(2,WBGym::grade() - 5);
-			
+
+		foreach ($arrAllLectures as $lecture) {
+
+			$bn = pow(2, WBGym::grade() - 5);
+
 			$lecture['hasDescription'] = ($lecture['title'] != $lecture['description']);
-			
-			if(!empty($arrLectionsLastYear) && in_array($lecture['id'],$arrLectionsLastYear)) {
+
+			if (!empty($arrLectionsLastYear) && in_array($lecture['id'], $arrLectionsLastYear)) {
 				$lecture['visitedLastYear'] = true;
 			}
-			
-			if ($lecture['classes'] & $bn) {
+
+			if ($lecture['classes']& $bn) {
 				$arrLectures[] = $lecture;
 			}
 		}
-		
+
 		$arrChoices = $this->Database->prepare("SELECT * FROM tl_tdwChoices WHERE student = ?")->execute($this->User->id)->fetchAllAssoc();
-		
+
 		$arrChoicesLectures = array();
-		if (count ($arrChoices) > 0 )
-			foreach($arrChoices as $choice)
+		if (count($arrChoices) > 0)
+			foreach ($arrChoices as $choice)
 				array_push($arrChoicesLectures, $choice['lecture']);
-		
-		$this->Template->userMaxChoiceAmount = $this->arrChoicesAmount[WBGym::grade()]; 
+
+		$this->Template->userMaxChoiceAmount = $this->arrChoicesAmount[WBGym::grade()];
 		$this->Template->arrLectures = $arrLectures;
 		$this->Template->arrChoices = $arrChoicesLectures;
 		$this->Template->User = $this->User;
-		
+
 	}
-		
+
 }
--- src/Resources/contao/modules/ModuleTdwChoiceAdmin.php
+++ src/Resources/contao/modules/ModuleTdwChoiceAdmin.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Contao Open Source CMS
@@ -42,40 +42,40 @@ class ModuleTdwChoiceAdmin extends \BackendModule
 	protected function compile()
 	{
 		$this->getStats();
-		if(strlen(\Input::post('storeChoicesInCsvFile'))) {
-			$this->storeChoicesInCsvFile();	
+		if (strlen(\Input::post('storeChoicesInCsvFile'))) {
+			$this->storeChoicesInCsvFile();
 			$this->Template->message = 'Export der Wünsche erfolgreich!';
 		}
-		if(strlen(\Input::post('importLecturesFromCsv'))) {
+		if (strlen(\Input::post('importLecturesFromCsv'))) {
 			$this->importLecturesFromCsv();
 			$this->Template->message = 'Import der Vorträge erfolgreich!';
 		}
-		if(strlen(\Input::post('copyLecturesToArticle'))) {
+		if (strlen(\Input::post('copyLecturesToArticle'))) {
 			$this->copyLecturesTo(\Input::post('copyArticle'));
 			$this->Template->message = 'Kopieren der Vorträge erfolgreich!';
 		}
-		
+
 		$this->Template->pages = $this->getArticles();
 		$this->Template->lectureCount = $this->lectureCount;
 		$this->Template->studentsHaveChosen = $this->studentsHaveChosen;
 		$this->Template->studentsHaveToChoose = $this->studentsHaveToChoose;
 	}
-	
-	protected function getStats() {
+
+	protected function getStats()
+	{
 		//Get number of different lectures
 		$this->lectureCount = $this->Database->prepare("SELECT count(*) FROM tl_tdwLectures")->execute()->fetchRow()[0];
-		
+
 		//Get number of Students who have already chosen
 		$this->studentsHaveChosen = $this->Database->prepare("SELECT count(DISTINCT student) FROM tl_tdwChoices")->execute()->fetchRow()[0];
-		 
+
 		//Get number of Students who have to choose altogether
-		$strCsvFile = TL_ROOT.'/files/tdw/input_files/T_Schueler_fuer_Schuelerwahl.csv';
-		if(file_exists($strCsvFile)) {
-			$delimiter = ';';
-			$handle = fopen($strCsvFile,'r');
+		$strCsvFile = TL_ROOT . '/files/tdw/input_files/T_Schueler_fuer_Schuelerwahl.csv';
+		if (file_exists($strCsvFile)) {
+			$handle = fopen($strCsvFile, 'r');
 			$count = 0;
 			$row = 1;
-			while(($data = fgetcsv($handle,1000,$delimiter)) !== false) {
+			while (($data = fgetcsv($handle, 1000, $this->csvSeperator)) !== false) {
 				if ($row > 1) {
 					$count++;
 				}
@@ -84,25 +84,29 @@ class ModuleTdwChoiceAdmin extends \BackendModule
 			$this->studentsHaveToChoose = $count;
 		}
 	}
-	
-	protected function storeChoicesInCsvFile() {
-		
+
+	protected function storeChoicesInCsvFile()
+	{
+
 		//build array which assigns every student id from our DB the new ID from CSV
 		$students = $this->Database->prepare("SELECT id,firstname,lastname,dateOfBirth FROM tl_member WHERE student")->execute();
-		
-		while($student = $students->fetchAssoc()) {
-			
-			$strCsvFile = TL_ROOT.'/files/tdw/input_files/T_Schueler_fuer_Schuelerwahl.csv';
-			if(file_exists($strCsvFile)) {
-				$delimiter = ';';
-				$handle = fopen($strCsvFile,'r');
+
+		while ($student = $students->fetchAssoc()) {
+
+			$strCsvFile = TL_ROOT . '/files/tdw/input_files/T_Schueler_fuer_Schuelerwahl.csv';
+			if (file_exists($strCsvFile)) {
+				$handle = fopen($strCsvFile, 'r');
 				$row = 1;
-				
-				while(($data = fgetcsv($handle,1000,$delimiter)) !== false) {
-					if ($row > 1 && 
-						$data[1] == $student['lastname'] && 
+
+				while (($data = fgetcsv($handle, 1000, $this->csvSeperator)) !== false) {
+					if (
+						$row > 1 &&
+						$data[1] == $student['lastname'] &&
 						/*$data[2] == $student['firstname'] && */
-						$data[3] == date('d.m.Y',$student['dateOfBirth'])
+						$data[3] == date(
+							'd.m.Y',
+							$student['dateOfBirth']
+						)
 					) {
 						$arrStIds[$student['id']] = $data[0];
 					}
@@ -110,121 +114,129 @@ class ModuleTdwChoiceAdmin extends \BackendModule
 				}
 			}
 		}
-		
+
 		//write every choice into new file
 		$choices = $this->Database->prepare("SELECT student, lecture FROM tl_tdwChoices")->execute();
-		$strData = implode($this->csvSeperator,array('Schueler','Vortrag')) . "\n";
-		   
-		while($choice = $choices->fetchAssoc()) {
-		
+		$strData = implode($this->csvSeperator, array('Schueler', 'Vortrag')) . "\n";
+
+		while ($choice = $choices->fetchAssoc()) {
+
 			$studentId = $choice['student'];
 			$lectureId = $choice['lecture'];
-			
-			if($arrStIds[$studentId]) {
-				$strData .= implode($this->csvSeperator,array($arrStIds[$studentId],$lectureId)) . "\n";
+
+			if ($arrStIds[$studentId]) {
+				$strData .= implode($this->csvSeperator, array($arrStIds[$studentId], $lectureId)) . "\n";
 			}
 		}
-                
-        $choicesCsvFile = fopen(TL_ROOT.'/files/tdw/export_files/choices.txt','w');
+
+		$choicesCsvFile = fopen(TL_ROOT . '/files/tdw/export_files/choices.txt', 'w');
 		fwrite($choicesCsvFile, $strData);
-        fclose($choicesCsvFile);
+		fclose($choicesCsvFile);
 	}
-	
-	protected function storeStudentsInCsvFile(){
-		
+
+	protected function storeStudentsInCsvFile()
+	{
+
 		$students = $this->Database->prepare("SELECT id, firstname, lastname FROM tl_member WHERE student")->execute();
-		
-		$strData = implode($this->csvSeperator,array('Id','Name')) . "\n";
-		
-		while($student = $students->fetchAssoc()) {
-			
-			
-			$strData .= implode($this->csvSeperator, array($student['id'],$student['firstname'] . ' ' . $student['lastname'])) . "\n";
-			
+
+		$strData = implode($this->csvSeperator, array('Id', 'Name')) . "\n";
+
+		while ($student = $students->fetchAssoc()) {
+
+
+			$strData .= implode($this->csvSeperator, array($student['id'], $student['firstname'] . ' ' . $student['lastname'])) . "\n";
+
 		}
-		
+
 		$objFile = new \File('files/tdw/export_files/students.csv');
 		$objFile->write($strData);
-		
+
 	}
-	
-	protected function storeLecturesInCsvFile(){
-		
+
+	protected function storeLecturesInCsvFile()
+	{
+
 		$lectures = $this->Database->prepare("SELECT id, title, description FROM tl_tdwLectures")->execute();
-		
-		$strData = implode($this->csvSeperator,array('Id','Titel','Beschreibung')) . "\n";
-		
-		while($lecture = $lectures->fetchAssoc()) {
-			
-			
-			$strData .= implode($this->csvSeperator, array($lecture['id'],$lecture['title'],$lecture['description'])) . "\n";
-			
+
+		$strData = implode($this->csvSeperator, array('Id', 'Titel', 'Beschreibung')) . "\n";
+
+		while ($lecture = $lectures->fetchAssoc()) {
+
+
+			$strData .= implode($this->csvSeperator, array($lecture['id'], $lecture['title'], $lecture['description'])) . "\n";
+
 		}
-		
+
 		$objFile = new \File('files/tdw/export_files/lectures.csv');
 		$objFile->write($strData);
-		
+
 	}
 
-	protected function importLecturesFromCsv() {
-		$this->Database->prepare("DELETE FROM tl_tdwLectures")->execute(); 
+	protected function importLecturesFromCsv()
+	{
+		$this->Database->prepare("DELETE FROM tl_tdwLectures")->execute();
 		$row = 1;
 
-		$strCsvFile = TL_ROOT.'/files/tdw/input_files/T_Vortragsthemen_fuer_Schuelerwahl.csv';
-		$delimiter = '###';
-		
-		$handle = fopen($strCsvFile,'r');
+		$strCsvFile = TL_ROOT . '/files/tdw/input_files/T_Vortragsthemen_fuer_Schuelerwahl.csv';
 
-		while(($data = fgetcsv($handle,1000,$delimiter)) !== false) {
-			
+		$handle = fopen($strCsvFile, 'r');
+
+		while (($data = fgetcsv($handle, 1000, $this->csvSeperator)) !== false) {
+
 			$num = count($data);
 
-			if($row > 1) {
-			
+			if ($row > 1) {
+
 				$bn = 0;
 				$arrColumns = array();
-			
-				for($c = 0; $c < $num; $c++) array_push($arrColumns,$data[$c]);
-				
-				/*Neue Variante mit Speaker*/ 
-				for ($i = 0; $i < 8; $i++) $bn = $bn + pow(2,$i) * !!$arrColumns[$i + 4];
-				
+
+				for ($c = 0; $c < $num; $c++)
+					array_push($arrColumns, $data[$c]);
+
+				/*Neue Variante mit Speaker*/
+				for ($i = 0; $i < 8; $i++)
+					$bn = $bn + pow(2, $i) * !!$arrColumns[$i + 4];
+
 				$lectureId = $arrColumns[0];
 				$lectureTitle = $arrColumns[1];
-				
+
 				/*Neue Variante mit Speaker*/
 				$lectureSpeaker = $arrColumns[2];
 				$lectureDescription = $arrColumns[3];
-				
+
 				$this->Database->prepare("INSERT INTO tl_tdwLectures (id, tstamp, title, speaker, description, classes) VALUES (?,?,?,?,?,?)")
-					   ->execute($lectureId,time(),$lectureTitle,$lectureSpeaker,$lectureDescription,$bn);
+					->execute($lectureId, time(), $lectureTitle, $lectureSpeaker, $lectureDescription, $bn);
 			}
-			
+
 			$row++;
 		}
 	}
-	
-	protected function copyLecturesTo($articleId) {
+
+	protected function copyLecturesTo($articleId)
+	{
 		$intSort = 0;
 		$lectures = $this->Database->prepare("SELECT * FROM tl_tdwLectures ORDER BY title")->execute();
 		while ($arrLecture = $lectures->fetchAssoc()) {
-			$intSort += pow(2,7);
-			
-			if($arrLecture['speaker']) $strHeadline = $arrLecture['speaker'] . ': ' . $arrLecture['title'];
-			else $strHeadline = $arrLecture['title'];
-			
+			$intSort += pow(2, 7);
+
+			if ($arrLecture['speaker'])
+				$strHeadline = $arrLecture['speaker'] . ': ' . $arrLecture['title'];
+			else
+				$strHeadline = $arrLecture['title'];
+
 			$this->Database->prepare("INSERT INTO tl_content (pid, type, sorting, tstamp, mooHeadline, text, ptable) VALUES (?,?,?,?,?,?,?)")
-							->execute($articleId,'accordionSingle',$intSort,time(),$strHeadline,$arrLecture['description'],'tl_article');
+				->execute($articleId, 'accordionSingle', $intSort, time(), $strHeadline, $arrLecture['description'], 'tl_article');
 		}
 	}
-	
-	protected function getArticles() {
+
+	protected function getArticles()
+	{
 		$articles = $this->Database->prepare("
 			SELECT p.id AS pageId, a.id AS articleId, p.title AS pageTitle, a.title AS articleTitle, a.inColumn 
 			FROM tl_page p JOIN tl_article a ON p.id = a.pid WHERE type = ? AND a.inColumn = ? ORDER BY p.pid, p.id, a.sorting; 
-		")->execute('regular','main');
-	
-		while($arrArticle = $articles->fetchAssoc()) {
+		")->execute('regular', 'main');
+
+		while ($arrArticle = $articles->fetchAssoc()) {
 			$arrArticles[$arrArticle['pageTitle']][$arrArticle['articleId']] = array('id' => $arrArticle['articleId'], 'title' => $arrArticle['articleTitle']);
 		}
 		return $arrArticles;
